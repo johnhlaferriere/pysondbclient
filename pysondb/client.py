b60e11e849f75f1485ede87142d95904d1622b23
John Laferriere <jlaferriere@nhrs.org>
2023-05-15 08:50:08 -0400

    formatting

--- pysondb/client.py
+++ pysondb/client.py
@@ -10,7 +10,7 @@ from typing import List
 from typing import Optional
 from typing import Union
 from typing import Dict
-#import pysondb.errors as errors
+
 from pysondb.db_types import DBSchemaType
 from pysondb.db_types import IdGeneratorType
 from pysondb.db_types import NewKeyValidTypes
@@ -33,9 +33,6 @@ except ImportError:
     import json as json
 
 
-
-
-
 class PysonDBClient:
     def __init__(self, host, port, wait=1):
         super().__init__
@@ -47,10 +44,98 @@ class PysonDBClient:
         self._dbname = None
         self._section = None
 
+    def _check_error(self, val):
+        if val["error"] == "NoError":
+            return val["data"]
+        else:
+            raise getattr(
+                getattr(__import__("pysondb.errors"), "errors"), val["error"]
+            )(val["data"])
 
-  
+    def _check_section(self, s) -> str:
+        if s == None and self._section == None:
+            raise (SectionNotFoundError("There is no section selected."))
+        return s if s != None else self._section
 
-        
+    def _recvall(self):
+        MAX_BUF = 1024
+        data = bytearray()
+        len = int.from_bytes(self._sock.recv(8), "big")
+        loop = len // MAX_BUF
+        while loop > 0:
+            data += self._sock.recv(MAX_BUF)
+            loop -= 1
+        data += self._sock.recv(len % MAX_BUF)
+        return data.decode("utf-8")
+
+    def _send(self, data: object, check: bool = True) -> str:
+        if self._connected:
+            if check and self._dbname == None:
+                raise Exception("No database has been selected.")
+            d = json.dumps(data)
+            self._sock.sendall(bytes(d + "\n", encoding="utf8"))
+            return json.loads(self._recvall())
+        else:
+            raise Exception("No Cennection")
+
+    def add(self, data: object, section: str = None) -> str:
+        return self._check_error(
+            self._send(
+                {
+                    "cmd": "ADD",
+                    "payload": {"section": self._check_section(section), "data": data},
+                }
+            )
+        )
+
+    def add_many(self, data: object, json_response: bool = True, section: str = None):
+        return self._check_error(
+            self._send(
+                {
+                    "cmd": "ADD_MANY",
+                    "payload": {
+                        "section": self._check_section(section),
+                        "data": data,
+                        "json_response": json_response,
+                    },
+                }
+            )
+        )
+
+    def add_new_key(
+        self, key: str, default: Optional[NewKeyValidTypes] = None, section: str = None
+    ):
+        return self._check_error(
+            self._send(
+                {
+                    "cmd": "ADD_NEW_KEY",
+                    "payload": {
+                        "section": self._check_section(section),
+                        "key": key,
+                        "default": default,
+                    },
+                }
+            )
+        )
+
+    def add_section(self, section: str = None, use: bool = True):
+        retval = self._check_error(
+            self._send(
+                {
+                    "cmd": "ADD_SECTION",
+                    "payload": {"section": self._check_section(section), "use": use},
+                }
+            )
+        )
+        if use:
+            self._section = retval
+        return retval
+
+    def close(self) -> None:
+        self._sock.shutdown(socket.SHUT_RDWR)
+        self._sock.close()
+        self._connected = False
+
     def connect(self, retries=5) -> bool:
         self._sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         if not self._connected:
@@ -64,125 +149,137 @@ class PysonDBClient:
                 rCount += 1
         return self._connected
 
-    def close(self) -> None:
-        self._sock.shutdown(socket.SHUT_RDWR)
-        self._sock.close()
-        self._connected = False
+    def create_db(self, dbname: str, use: bool = True, force: bool = False):
+        retval = self._check_error(
+            self._send(
+                {
+                    "cmd": "CREATE_DB",
+                    "payload": {"dbname": dbname, "use": use, "force": force},
+                },
+                False,
+            )
+        )
+        self._dbname = dbname
+        return retval
 
-    def recvall(self):
-        MAX_BUF = 1024
-        data = bytearray()
-        len = int.from_bytes(self._sock.recv(8), "big")
-        loop = len // MAX_BUF
-        while loop > 0:
-            data += self._sock.recv(MAX_BUF)
-            loop -= 1
-        data += self._sock.recv(len % MAX_BUF)
-        return data.decode("utf-8")
-
-    def _send(self, data: object,check:bool = True) -> str:
-      
-        if self._connected:
-            if check and self._dbname == None: 
-                raise Exception("No database has been selected.")
-            d = json.dumps(data)
-            self._sock.sendall(bytes(d + "\n", encoding="utf8"))
-            return json.loads(self.recvall())
-        else:
-            raise Exception("No Cennection")
-
-    def _check_error(self, val):
-        if val["error"] == "NoError":
-            return val["data"]
-        else:
-            raise getattr(getattr(__import__("pysondb.errors"),"errors"), val["error"])(val["data"])
-
-    def _check_section(self,s) -> str:    
-        if s == None and self._section == None:
-            raise(SectionNotFoundError("There is no section selected."))
-        return s if s != None else self._section
- 
-    def add(self, data: object,section:str = None) -> str:
-        return self._check_error(self._send({"cmd": "ADD", "payload":{"section":self._check_section(section),"data": data}}))
-
-    def add_many(self, data: object,json_response: bool = True, section : str = None):
+    def delete_by_id(self, id: str, section: str = None):
         return self._check_error(
             self._send(
                 {
-                    "cmd": "ADD_MANY",
-                    "payload": {"section":self._check_section(section),"data": data, "json_response": json_response},
+                    "cmd": "DELETE_BY_ID",
+                    "payload": {"section": self._check_section(section), "id": id},
                 }
             )
         )
 
-    def add_new_key(self, key: str, default: Optional[NewKeyValidTypes] = None, section: str = None):
+    def delete_by_query(self, query, section: str = None):
         return self._check_error(
             self._send(
-                {"cmd": "ADD_NEW_KEY", "payload": {"section":self._check_section(section),"key": key, "default": default}}
+                {
+                    "cmd": "DELETE_BY_QUERY",
+                    "payload": {
+                        "section": self._check_section(section),
+                        "query": query,
+                    },
+                }
             )
         )
-
-    def add_section(self,section:str = None,use : bool = True):
-         retval = self._check_error(self._send({"cmd": "ADD_SECTION", "payload": {"section":self._check_section(section),"use":use}}))
-         if use :
-            self._section = retval
-         return retval
 
     def get_all(self) -> str:
         return self._check_error(self._send({"cmd": "GET_ALL", "payload": ""}))
 
-    def get_all_by_section(self,section:str = None) -> str:
-        return self._check_error(self._send({"cmd": "GET_ALL_BY_SECTION", "payload": {"section":self._check_section(section)}}))
+    def get_all_by_section(self, section: str = None) -> str:
+        return self._check_error(
+            self._send(
+                {
+                    "cmd": "GET_ALL_BY_SECTION",
+                    "payload": {"section": self._check_section(section)},
+                }
+            )
+        )
 
-    def get_by_id(self, id: str,section:str = None):
-        return self._check_error(self._send({"cmd": "GET_BY_ID", "payload": {"section":self._check_section(section),"id": id}}))
+    def get_by_id(self, id: str, section: str = None):
+        return self._check_error(
+            self._send(
+                {
+                    "cmd": "GET_BY_ID",
+                    "payload": {"section": self._check_section(section), "id": id},
+                }
+            )
+        )
 
-    def get_by_query(self, query: str,  section:str = None):
-        return self._check_error(self._send({"cmd": "GET_BY_QUERY", "payload": {"section":self._check_section(section),"query":query}}))
+    def get_by_query(self, query: str, section: str = None):
+        return self._check_error(
+            self._send(
+                {
+                    "cmd": "GET_BY_QUERY",
+                    "payload": {
+                        "section": self._check_section(section),
+                        "query": query,
+                    },
+                }
+            )
+        )
 
-    def update_by_id(self, id: str, new_data: object,section:str = None):
-        return self._check_error(self._send(
-            {"cmd": "UPDATE_BY_ID", "payload": {"section":self._check_section(section), "id": id, "data": new_data}}
-        ))
-
-    def update_by_query(self, query: str, new_data: object,section:str = None):
-        return self._check_error(self._send(
-            {"cmd": "UPDATE_BY_QUERY", "payload": {"section":self._check_section(section),"query": query, "data": new_data}}
-        ))
-
-    def delete_by_id(self, id: str, section:str = None):
-        return self._check_error(self._send({"cmd": "DELETE_BY_ID", "payload": {"section":self._check_section(section),"id": id}}))
-
-    def delete_by_query(self, query,section:str = None):
-        return self._check_error(self._send({"cmd": "DELETE_BY_QUERY", "payload": {"section":self._check_section(section),"query": query}}))
-
-    def purge(self,section:str = None):
-        return self._check_error(self._send({"cmd": "PURGE", "payload": {"section":self._check_section(section)}}))
+    def purge(self, section: str = None):
+        return self._check_error(
+            self._send(
+                {"cmd": "PURGE", "payload": {"section": self._check_section(section)}}
+            )
+        )
 
     def purge_all(self):
         return self._check_error(self._send({"cmd": "PURGE_ALL", "payload": ""}))
 
-    def use_db(self,dbname:str,section:str = None):
-        retval = self._check_error(self._send({"cmd": "USE_DB", "payload": {"dbname":dbname,"section":section}},False))
+    def set_id_generator(self, fn: any):
+        return self._check_error(
+            self._send({"cmd": "SET_ID_GENERATOR", "payload": {"fn": fn}})
+        )
+
+    def update_by_id(self, id: str, new_data: object, section: str = None):
+        return self._check_error(
+            self._send(
+                {
+                    "cmd": "UPDATE_BY_ID",
+                    "payload": {
+                        "section": self._check_section(section),
+                        "id": id,
+                        "data": new_data,
+                    },
+                }
+            )
+        )
+
+    def update_by_query(self, query: str, new_data: object, section: str = None):
+        return self._check_error(
+            self._send(
+                {
+                    "cmd": "UPDATE_BY_QUERY",
+                    "payload": {
+                        "section": self._check_section(section),
+                        "query": query,
+                        "data": new_data,
+                    },
+                }
+            )
+        )
+
+    def use_db(self, dbname: str, section: str = None):
+        retval = self._check_error(
+            self._send(
+                {"cmd": "USE_DB", "payload": {"dbname": dbname, "section": section}},
+                False,
+            )
+        )
         self._dbname = dbname
         if section != None:
             self._section = section
-         
 
-    def use_section(self,section:str):
+    def use_section(self, section: str):
         if section == self._section:
             return
-        retval = self._check_error(self._send({"cmd":"USE_SECTION","payload":{"section":section}}))
+        retval = self._check_error(
+            self._send({"cmd": "USE_SECTION", "payload": {"section": section}})
+        )
         self._section = section
         return section
-
-    def create_db(self,dbname:str,use :bool = True,force :bool = False):
-        retval = self._check_error(self._send({"cmd": "CREATE_DB", "payload": {"dbname":dbname,"use":use,"force":force}},False))
-        self._dbname = dbname
-        return retval
-
-    def set_id_generator(self,fn :any):
-        return self._check_error(self._send({"cmd":"SET_ID_GENERATOR","payload":{"fn":fn}}))
-
-
-    
